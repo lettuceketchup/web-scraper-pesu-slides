148a522b62c5168011084edb2a088e532c85d3f2
lettuceketchup <rishishwarmanu@gmail.com>
2020-12-21 15:18:53 +0530

    Creating new file

---
+++ pesu_slides.py
@@ -0,0 +1,9 @@
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from colorama import init, deinit
+from colors import task, success, error, info, info_cont
